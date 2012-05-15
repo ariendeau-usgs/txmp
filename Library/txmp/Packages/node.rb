8c53be642a862492669ad3768eb43495a91fc1d7
Austin Riendeau <ariendeau@usgs.gov>
2012-05-15 11:10:37 -0600

    added nodejs bundle

---
+++ Library/txmp/Packages/node.rb
@@ -0,0 +1,7 @@
+@name = "node"
+@description = "A NodeJS bundle"
+@filename = "node.tmbundle"
+@url = "https://github.com/visionmedia/node.tmbundle.git"
+@author="visionmedia"
+
+@catagory = "Bundle"
