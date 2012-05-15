1afe2b0d7cf3db1adbbe0a4517b49ab3ae50f1ec
Austin Riendeau <ariendeau@usgs.gov>
2012-05-15 11:12:59 -0600

    added ruby-on-rails

---
+++ Library/txmp/Packages/ruby-on-rails.rb
@@ -0,0 +1,7 @@
+@name = "ruby-on-rails"
+@description = "A Ruby on Rails bundle"
+@filename = "ruby-on-rails.tmbundle"
+@url = "https://github.com/drnic/ruby-on-rails-tmbundle.git"
+@author="drnic"
+
+@catagory = "Bundle"
